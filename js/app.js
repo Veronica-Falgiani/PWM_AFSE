c6db1112479a9e85c67e5a552699290ce0afd7fa
Sakurai777 <veronica03f@gmail.com>
2025-01-17 12:42:07 +0100

    Backup of all files

---
+++ js/app.js
@@ -0,0 +1,5 @@
+//const mongoClient = require('mongodb').MongoClient;
+//const ObjectId = require('mongodb').ObjectId;
+var uri = "mongodb+srv://veronicafalgiani:NEPuCX3YD0DpAB19@afsm.wit5h.mongodb.net/";
+
+/* Register user */
---
+++ js/menu.js
@@ -0,0 +1,62 @@
+var sitename = location.pathname.substring(location.pathname.lastIndexOf("/") + 1);
+var credits = 300
+
+if (sitename == "index.html" || sitename == "login.html" || sitename == "register.html") {
+    const menuElement = document.getElementById('menu');
+    menuElement.innerHTML = 
+    `
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <nav class="navbar sticky-top navbar-expand-md bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.html">AFSE</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+    </nav>
+    `;
+} 
+else {
+    const menuElement = document.getElementById('menu');
+    menuElement.innerHTML = 
+    `
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <nav class="navbar sticky-top navbar-expand-md bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">AFSE</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="trade.html">Scambi</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="credits.html">Crediti</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="packs.html">Pacchetti</a>
+                    </li>
+                    
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown">
+                            "NOME"
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="profile.html">Profilo</a></li>
+                            <li><a class="dropdown-item" href="album.html">Album</a></li>
+                            <li><a class="dropdown-item" href="album.html">Crediti totali: ${credits}</a></li>
+                            <li><hr></li>
+                            <li><a class="dropdown-item text-danger" href="#">Logout</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+    </div>
+
+    </nav>
+    `;
+}
