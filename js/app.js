a6ed753af00d1408f939f7874d09457d79748b16
Sakurai777 <veronica03f@gmail.com>
2025-01-17 12:46:36 +0100

    Changed images

--- js/app.js
+++ js/app.js
@@ -3,3 +3,23 @@
 var uri = "mongodb+srv://veronicafalgiani:NEPuCX3YD0DpAB19@afsm.wit5h.mongodb.net/";
 
 /* Register user */
+
+/* Example node 
+//Load HTTP module
+const http = require("http");
+const hostname = "127.0.0.1";
+const port = 3000;
+
+//Create HTTP server and listen on port 3000 for requests
+const server = http.createServer((req, res) => {
+  //Set the response HTTP header with HTTP status and Content type
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello world\n");
+});
+
+//listen for request on port 3000, and as a callback function have the port listened on logged
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+*/
