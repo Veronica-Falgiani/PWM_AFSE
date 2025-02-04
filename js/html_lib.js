8e74f0182a9aa03d1cc89246cd672f854f9a7faf
Sakurai777 <veronica03f@gmail.com>
2025-02-04 18:43:22 +0100

    Finished all the functions for the album

--- js/html_lib.js
+++ js/html_lib.js
@@ -155,11 +155,84 @@ function writeCards(heroes) {
 }
 
 /* ALBUM.HTML*/
+/* Updates all the values to show the user cards */
+function showUserCards() {
+    pageMin = 1
+    pageMax = Math.ceil(userCards.length / 50)
+    page = 1
+
+    document.getElementById("currentPageUp").innerHTML = `${page}/${pageMax}`
+        document.getElementById("currentPageDown").innerHTML = `${page}/${pageMax}`
+
+    populateUserCards(userCards, page)
+}
+
+/* Updates all the values to show all the cards */
+function showCards() {
+    pageMin = 1
+    pageMax = 32
+    page = 1
+
+    document.getElementById("currentPageUp").innerHTML = `${page}/${pageMax}`
+        document.getElementById("currentPageDown").innerHTML = `${page}/${pageMax}`
+
+    populateCards(allCards, userCards, page)
+}
+
 /* Prints 50 cards for the current page */
-function populateCards(cards, page) {
+function populateCards(allCards, userCards, page) {
     /* Gets 50 cards based on the current page */
     end = page * 50 // 100
     start = end - 50 // 50
+    found = 0
+    num = 0
+
+    document.getElementById("selectButton").innerHTML = `<button class="btn btn-primary" onclick="showUserCards()">Mostra carte utente</button>`
+
+    /* Clears the html to be repopulated */
+    const cardsRow = document.getElementById("cards");
+    cardsRow.innerHTML = ``
+
+    /* Populate with new cards */
+    for(i = start; i < end; i++) {
+        for(j = 0; j < userCards.length; j++) {
+            if(allCards[i].id == userCards[j].id) {
+                found = 1
+                num = userCards[j].number
+            }
+        }
+        /* If the card is not found in the user db it's grayed out */
+        if (found == 0) {
+            cardsRow.innerHTML += 
+            `
+            <div class="col p-3 m-3 border rounded">
+                <img class="mb-3" src="${allCards[i].thumbnail}" width="100px" height="100px" id="unobtained" style="filter: grayscale(100%)">  
+                <p> ${allCards[i].name}</p>
+            </div>
+            `;
+        }
+        /* If the card is found in the user db it is clickable and shows the amount */
+        else {
+            cardsRow.innerHTML += 
+            `
+            <button class="col btn-danger p-3 m-3 border rounded" onclick="getCard(${allCards[i].id})">
+                <img class="mb-3" src="${allCards[i].thumbnail}" width="100px" height="100px">  
+                <p> (${num}) ${allCards[i].name}</p>
+            </button>
+            `;
+            found = 0
+        }
+    }
+}
+
+/* Prints 50 cards of the user */
+function populateUserCards(userCards, page) {
+    /* Gets 50 cards based on the current page */
+    end = page * 50 // 100
+    start = end - 50 // 50
+
+    document.getElementById("selectButton").innerHTML = `<button class="btn btn-primary" onclick="showCards()">Mostra tutte le carte</button>`
+    // TODO ADD USER CARDS CLICKABEL AND THE OTHERS NOT
 
     /* Clears the html to be repopulated */
     const cardsRow = document.getElementById("cards");
@@ -169,11 +242,31 @@ function populateCards(cards, page) {
     for(i = start; i < end; i++) {
         cardsRow.innerHTML += 
         `
-        <button class="col p-3 m-3 border rounded" onclick="getCard(${cards[i].id})">
-            <img class="mb-3" src="${cards[i].thumbnail}" width="100px" height="100px">  
-            <p> ${cards[i].name}</p>
+        <button class="col p-3 m-3 border rounded" onclick="getCard(${allCards[i].id})">
+            <img class="mb-3" src="${userCards[i].thumbnail}" width="100px" height="100px">  
+            <p> (${userCards[i].number}) ${userCards[i].name}</p>
         </button>
         `;
+    }
+}
+
+/* Function to go to next page and populate it */
+function prev() {
+    if (page > 1) {
+        page = page - 1
+        document.getElementById("currentPageUp").innerHTML = `${page}/${pageMax}`
+        document.getElementById("currentPageDown").innerHTML = `${page}/${pageMax}`
+        populateCards(allCards, userCards, page)
+    }
+}
+
+/* Function to go to previous page and populate it */
+function next() {
+    if (page < pageMax) {
+        page = page + 1
+        document.getElementById("currentPageUp").innerHTML = `${page}/${pageMax}`
+        document.getElementById("currentPageDown").innerHTML = `${page}/${pageMax}`
+        populateCards(allCards, userCards, page)
     }
 }
 
