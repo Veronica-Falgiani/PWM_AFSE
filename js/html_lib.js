2714f5a6fd46632ad345815b66b3aaad374ea533
Sakurai777 <veronica03f@gmail.com>
2025-02-13 15:13:30 +0100

    moved getFromMarvel, fixed register, login and profile

--- js/html_lib.js
+++ js/html_lib.js
@@ -1,38 +1,61 @@
-/* Main function to talk to the server */
-function getFromMarvel(url, query="") {
-    var MD5 = function(d){var r = M(V(Y(X(d),8*d.length)));return r.toLowerCase()};function M(d){for(var _,m="0123456789ABCDEF",f="",r=0;r<d.length;r++)_=d.charCodeAt(r),f+=m.charAt(_>>>4&15)+m.charAt(15&_);return f}function X(d){for(var _=Array(d.length>>2),m=0;m<_.length;m++)_[m]=0;for(m=0;m<8*d.length;m+=8)_[m>>5]|=(255&d.charCodeAt(m/8))<<m%32;return _}function V(d){for(var _="",m=0;m<32*d.length;m+=8)_+=String.fromCharCode(d[m>>5]>>>m%32&255);return _}function Y(d,_){d[_>>5]|=128<<_%32,d[14+(_+64>>>9<<4)]=_;for(var m=1732584193,f=-271733879,r=-1732584194,i=271733878,n=0;n<d.length;n+=16){var h=m,t=f,g=r,e=i;f=md5_ii(f=md5_ii(f=md5_ii(f=md5_ii(f=md5_hh(f=md5_hh(f=md5_hh(f=md5_hh(f=md5_gg(f=md5_gg(f=md5_gg(f=md5_gg(f=md5_ff(f=md5_ff(f=md5_ff(f=md5_ff(f,r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+0],7,-680876936),f,r,d[n+1],12,-389564586),m,f,d[n+2],17,606105819),i,m,d[n+3],22,-1044525330),r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+4],7,-176418897),f,r,d[n+5],12,1200080426),m,f,d[n+6],17,-1473231341),i,m,d[n+7],22,-45705983),r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+8],7,1770035416),f,r,d[n+9],12,-1958414417),m,f,d[n+10],17,-42063),i,m,d[n+11],22,-1990404162),r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+12],7,1804603682),f,r,d[n+13],12,-40341101),m,f,d[n+14],17,-1502002290),i,m,d[n+15],22,1236535329),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+1],5,-165796510),f,r,d[n+6],9,-1069501632),m,f,d[n+11],14,643717713),i,m,d[n+0],20,-373897302),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+5],5,-701558691),f,r,d[n+10],9,38016083),m,f,d[n+15],14,-660478335),i,m,d[n+4],20,-405537848),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+9],5,568446438),f,r,d[n+14],9,-1019803690),m,f,d[n+3],14,-187363961),i,m,d[n+8],20,1163531501),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+13],5,-1444681467),f,r,d[n+2],9,-51403784),m,f,d[n+7],14,1735328473),i,m,d[n+12],20,-1926607734),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+5],4,-378558),f,r,d[n+8],11,-2022574463),m,f,d[n+11],16,1839030562),i,m,d[n+14],23,-35309556),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+1],4,-1530992060),f,r,d[n+4],11,1272893353),m,f,d[n+7],16,-155497632),i,m,d[n+10],23,-1094730640),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+13],4,681279174),f,r,d[n+0],11,-358537222),m,f,d[n+3],16,-722521979),i,m,d[n+6],23,76029189),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+9],4,-640364487),f,r,d[n+12],11,-421815835),m,f,d[n+15],16,530742520),i,m,d[n+2],23,-995338651),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+0],6,-198630844),f,r,d[n+7],10,1126891415),m,f,d[n+14],15,-1416354905),i,m,d[n+5],21,-57434055),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+12],6,1700485571),f,r,d[n+3],10,-1894986606),m,f,d[n+10],15,-1051523),i,m,d[n+1],21,-2054922799),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+8],6,1873313359),f,r,d[n+15],10,-30611744),m,f,d[n+6],15,-1560198380),i,m,d[n+13],21,1309151649),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+4],6,-145523070),f,r,d[n+11],10,-1120210379),m,f,d[n+2],15,718787259),i,m,d[n+9],21,-343485551),m=safe_add(m,h),f=safe_add(f,t),r=safe_add(r,g),i=safe_add(i,e)}return Array(m,f,r,i)}function md5_cmn(d,_,m,f,r,i){return safe_add(bit_rol(safe_add(safe_add(_,d),safe_add(f,i)),r),m)}function md5_ff(d,_,m,f,r,i,n){return md5_cmn(_&m|~_&f,d,_,r,i,n)}function md5_gg(d,_,m,f,r,i,n){return md5_cmn(_&f|m&~f,d,_,r,i,n)}function md5_hh(d,_,m,f,r,i,n){return md5_cmn(_^m^f,d,_,r,i,n)}function md5_ii(d,_,m,f,r,i,n){return md5_cmn(m^(_|~f),d,_,r,i,n)}function safe_add(d,_){var m=(65535&d)+(65535&_);return(d>>16)+(_>>16)+(m>>16)<<16|65535&m}function bit_rol(d,_){return d<<_|d>>>32-_}
-    var timestamp = Date.now();
-    var publicApiKey = "d5c6dc14448d8d7bda781bb3315ccdd3"
-    var privateApiKey = "1e433cea650dfd4fcc9cce795d7188f7c29b04df"
-    var parameters = `ts=${timestamp}&apikey=${publicApiKey}&hash=${MD5(timestamp+privateApiKey+publicApiKey)}&`
-  
-    console.log(`Request string: http://gateway.marvel.com/v1/public/${url}?${parameters}${query}`)
-
-    return fetch(`http://gateway.marvel.com/v1/public/${url}?${parameters}${query}`)
-    .then(response => response.json())
-    .catch(error => console.log('error', error));
-}
-
 /* MENU */
 /* Unsets all the localstorage items to "logout" user */
 function logoutUser() {
     localStorage.clear();
-    window.location.href = "http://localhost:3100/";
+    window.location.href = "/";
 }
 
 /* REGISTER.HTML */
+/* */
+async function searchHero() {
+    name = document.getElementById("selectHero").value;
+    localStorage.setItem("heroName", name)
+
+    await fetch("/marvelAPI", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json",
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({ "urlAPI" : `/characters`,
+                            "query" : `nameStartsWith=${name}`})
+    })
+        .then(response => response.json()).then(result => writeSelectHeroes(result))
+        .catch(error => console.log('error', error));
+}  
+
+/* Same as above but with series */
+async function searchSeries() {
+    title = document.getElementById("selectSeries").value
+    localStorage.setItem("seriesName", title)
+        
+    await fetch("/marvelAPI", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json",
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({ "urlAPI" : `/series`,
+                            "query" : `titleStartsWith=${title}`})
+    })
+        .then(response => response.json()).then(result => writeSelectSeries(result))
+        .catch(error => console.log('error', error));
+}
+
 /* creates a select menu with a list of heroes */
 function writeSelectHeroes(heroesJson) {
     heroesJson = (JSON.stringify(heroesJson));
     const heroes = JSON.parse(heroesJson);
 
     var inputHero = document.getElementById("inputHero")
+    inputHero.innerHTML = ``
+
     var lenHeroes = heroes.length
     for (i = 0; i < lenHeroes; i++) {
-        var opt = document.createElement("option")
-        opt.value = heroes[i].id
-        opt.innerHTML = heroes[i].name
-        inputHero.appendChild(opt)
+        inputHero.innerHTML += 
+        `
+        <option value="${heroes[i].id}">${heroes[i].name}</option>
+        `
     }
 }
 
@@ -42,13 +65,74 @@ function writeSelectSeries(seriesJson) {
     const series = JSON.parse(seriesJson);
 
     var inputSeries = document.getElementById("inputSeries")
+    inputSeries.innerHTML = ``
+
     var lenSeries = series.length
     for (i = 0; i < lenSeries; i++) {
-        var opt = document.createElement("option")
-        opt.value = series[i].id
-        opt.innerHTML = series[i].title
-        inputSeries.appendChild(opt)
+        inputSeries.innerHTML += 
+        `
+        <option value="${series[i].id}">${series[i].title}</option>
+        `
     }
+}
+
+/* Send the form to node and registers the user */
+async function sendForm() {
+    username = document.getElementById("username").value
+    email = document.getElementById("inputEmail").value
+    password = document.getElementById("inputPassword").value
+    hero = document.getElementById("inputHero").value
+    series = document.getElementById("inputSeries").value
+    localStorage.removeItem("heroName")
+    localStorage.removeItem("seriesName")
+
+    fetch("/register", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json",
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({ "username" : username, 
+                               "email" : email,
+                               "password" : password,
+                               "hero" : hero,
+                               "series" : series })
+        })
+        .then(result => result.json()).then(res => {
+            localStorage.setItem("username", res.username)
+            localStorage.setItem("email", res.email)
+            localStorage.setItem("credits", res.credits)
+
+            hero = fetch("/marvelAPI", {
+                method: "POST",
+                headers: {
+                    "Content-type": "application/json",
+                    'Accept': 'application/json',
+                },
+                body: JSON.stringify({ "urlAPI" : `characters/${res.hero}`,
+                                    "query" : ""})
+            })
+                .then(response => response.json()).then(result => {localStorage.setItem("hero", result[0].name)})
+                .catch(error => console.log('error', error));
+
+            series = fetch("/marvelAPI", {
+                method: "POST",
+                headers: {
+                    "Content-type": "application/json",
+                    'Accept': 'application/json',
+                },
+                body: JSON.stringify({ "urlAPI" : `series/${res.series}`,
+                                    "query" : ""})
+            })
+                .then(response => response.json()).then(result => {localStorage.setItem("series", result[0].title)})
+                .catch(error => console.log('error', error));
+    
+            /* window.location.href doesn't wait for the function to finish */
+            setTimeout(function(){
+                window.location.href = "/profile";
+            }, 3000);
+        })
+        .catch(error => console.log("Errore nella registrazione dell'utente", error));
 }
 
 /* PROFILE.HTML */
@@ -76,6 +160,42 @@ function writeProfile() {
     `
 }
 
+/* Modifies the info of the user */
+async function modifyUser() {
+    username = localStorage.getItem("username")
+    email = document.getElementById("inputEmail").value
+    password = document.getElementById("inputPassword").value
+
+    fetch(`/user/${username}`, {
+        method: "PUT",
+        headers: {
+            "Content-type": "application/json",
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({ "email" : email, 
+                               "password" : password })
+        })
+        .then(result => result.json()).then(res => {
+            localStorage.setItem("email", res.email)
+            window.location.href = "/profile";
+        })
+        .catch(error => console.log('Error updating the user', error));
+}
+
+/* Deletes the current user */
+async function deleteUser() { 
+    username = localStorage.getItem("username")
+
+    fetch(`/user/${username}`, {
+        method: "DELETE",
+        headers: {
+            "Content-type": "application/json",
+            'Accept': 'application/json',
+        }})
+        .then(result => result.json()).then(res => {localStorage.clear(); window.location.href = "/"})
+        .catch(error => console.log('Error deleting the user', error));
+}
+
 /* CREDITS.HTML */
 /* Populates the euros field inside the modal and saves it in the localStorage*/
 function setCredits(eur) {
@@ -89,7 +209,7 @@ async function addCredits() {
     creditsToAdd = localStorage.getItem("creditsToAdd")
     username = localStorage.getItem("username")
 
-    await fetch("http://localhost:3100/credits", {
+    await fetch("/credits", {
         method: "POST",
         headers: {
             "Content-type": "application/json",
@@ -101,7 +221,7 @@ async function addCredits() {
         .then(response => response.json()).then(res => {
             localStorage.removeItem("euros")
             crediti = localStorage.setItem("credits", res)
-            document.getElementById("crediti").innerHTML = `Totale crediti: ${res}`
+            document.getElementById("credits").innerHTML = `Totale crediti: ${res}`
         })
         .catch(error => console.log('error', error));
 }
@@ -119,8 +239,17 @@ async function getRandomHero() {
     maxHero = 1564;
     num = Math.floor(Math.random() * maxHero);
 
-    hero = getFromMarvel("/characters", `limit=1&offset=${num}`).then(result => {return result.data.results[0]})
-    console.log(hero)
+    hero = await fetch("/marvelAPI", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json",
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({ "urlAPI" : "/characters",
+                            "query" : `limit=1&offset=${num}`})
+    })
+        .then(response => response.json()).then(res => {return res[0]})
+        .catch(error => console.log('error', error));
 
     return hero
 }
@@ -359,7 +488,7 @@ async function sellCard() {
 
     console.log(id, username)
 
-    await fetch(`http://localhost:3100/card/${id}`, {
+    await fetch(`/card/${id}`, {
         method: "DELETE",
         headers: {
             "Content-type": "application/json",
@@ -481,9 +610,20 @@ function populateUserTrades(username, trades, page) {
     }
 }
 
-function updateRecHero() {
+async function updateRecHero() {
     recHero = document.getElementById("recHero").value
-    getFromMarvel("characters", `nameStartsWith=${recHero}`).then(result => updateTradeRec(result.data.results));
+    
+    hero = await fetch("/marvelAPI", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json",
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({ "urlAPI" : "/characters",
+                            "query" : `nameStartsWith=${recHero}`})
+    })
+        .then(response => response.json()).then(res => {updateTradeRec(res[0])})
+        .catch(error => console.log('error', error));
 }  
 
 /* Receives the info of the searched hero to receive and displays it */
@@ -529,7 +669,7 @@ async function updateSendHero() {
     username = localStorage.getItem("username")
     heroes = []
 
-    userCards = await fetch(`http://localhost:3100/cards/${username}`, {
+    userCards = await fetch(`/cards/${username}`, {
         method: "GET",
         headers: {
             "Content-type": "application/json",
