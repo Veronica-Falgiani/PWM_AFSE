f10bc57a75b8fd8033ecccd8fcabe18dff1699d9
Sakurai777 <veronica03f@gmail.com>
2025-02-16 08:50:51 +0100

    Fixed error codes

--- src/html/scripts/login.js
+++ src/html/scripts/login.js
@@ -15,142 +15,50 @@ async function sendForm() {
         body: JSON.stringify({ "username" : username, 
                                "password" : password })
         })
-        .then(result => result.json()).then(res => {
-            localStorage.setItem("username", res.username)
-            localStorage.setItem("email", res.email)
-            localStorage.setItem("credits", res.credits)
-
-            hero = fetch("/marvelAPI", {
-                method: "POST",
-                headers: {
-                    "Content-type": "application/json",
-                    'Accept': 'application/json',
-                },
-                body: JSON.stringify({ "urlAPI" : `characters/${res.hero}`,
-                                    "query" : ""})
-            })
-                .then(response => response.json()).then(result => {localStorage.setItem("hero", result[0].name)})
-                .catch(error => console.log('error', error));
-
-            series = fetch("/marvelAPI", {
-                method: "POST",
-                headers: {
-                    "Content-type": "application/json",
-                    'Accept': 'application/json',
-                },
-                body: JSON.stringify({ "urlAPI" : `series/${res.series}`,
-                                    "query" : ""})
-            })
-                .then(response => response.json()).then(result => {localStorage.setItem("series", result[0].title)})
-                .catch(error => console.log('error', error));
-    
-            /* window.location.href doesn't wait for the function getFromMarvel to finish */
-            setTimeout(function(){
-                window.location.href = "/profile";
-            }, 3000);
-        })
-        .catch(error => window.alert("Errore nel login dell'utente", error));
-}
-
-/*
-/* LOGIN.HTML 
-/* Collects the info from the form and logs in the user with the credentials 
-fetch("/login", {
-    method: "POST",
-    headers: {
-        "Content-type": "application/json",
-        'Accept': 'application/json',
-    },
-    body: JSON.stringify({ "username" : username, 
-                           "password" : password })
-    })
-    .then(result => {
-        if(result.ok) {
-            result.json().then(res => {
-                localStorage.setItem("username", res.username)
-                localStorage.setItem("email", res.email)
-                localStorage.setItem("credits", res.credits)
-
-                hero = fetch("/marvelAPI", {
-                    method: "POST",
-                    headers: {
-                        "Content-type": "application/json",
-                        'Accept': 'application/json',
-                    },
-                    body: JSON.stringify({ "urlAPI" : `characters/${res.hero}`,
-                                        "query" : ""})
-                })
-                .then(response => {
-                    if(response.ok) {
-                        response.json().then(result => {
-                            localStorage.setItem("hero", result[0].name)
-                        })
-                    }
-                    else {
-                        response.json().then(errorMessage => {
-                            document.getElementById("alert").innerHTML = 
-                            `
-                            <div class="alert alert-success" role="alert">
-                                <h5 class="alert-heading">${errorMessage}</h5>
-                            </div>
-                            `
-                            return
-                        });
-                    }
-                })
-                .catch(error => console.log('Error: ', error));
-
-                series = fetch("/marvelAPI", {
-                    method: "POST",
-                    headers: {
-                        "Content-type": "application/json",
-                        'Accept': 'application/json',
-                    },
-                    body: JSON.stringify({ "urlAPI" : `series/${res.series}`,
-                                        "query" : ""})
-                })
-                .then(response => {
-                    if(response.ok) {
-                        response.json().then(result => {
-                            localStorage.setItem("hero", result[0].title)
-                        })
-                    }
-                    else {
-                        response.text().then(errorMessage => {
-                            document.getElementById("alert").innerHTML = 
-                            `
-                            <div class="alert alert-success" role="alert">
-                                <h5 class="alert-heading">${errorMessage}</h5>
-                            </div>
-                            `
-                            return
-                        });
-                    }
-                })
-            })
-        }
+        .then(result => {
+            if(result.ok) {
+                result.json().then(res => {
+                    console.log(res)
+                    localStorage.setItem("username", res.username)
+                    localStorage.setItem("email", res.email)
+                    localStorage.setItem("credits", res.credits)
+        
+                    hero = fetch("/marvelAPI", {
+                        method: "POST",
+                        headers: {
+                            "Content-type": "application/json",
+                            'Accept': 'application/json',
+                        },
+                        body: JSON.stringify({ "urlAPI" : `characters/${res.hero}`,
+                                            "query" : ""})
+                    })
+                        .then(response => response.json()).then(result => {localStorage.setItem("hero", result[0].name)})
+                        .catch(error => alert("Error receiving a reponse from the Marvel API"));
+        
+                    series = fetch("/marvelAPI", {
+                        method: "POST",
+                        headers: {
+                            "Content-type": "application/json",
+                            'Accept': 'application/json',
+                        },
+                        body: JSON.stringify({ "urlAPI" : `series/${res.series}`,
+                                            "query" : ""})
+                    })
+                        .then(response => response.json()).then(result => {localStorage.setItem("series", result[0].title)})
+                        .catch(error => "Error receiving a reponse from the Marvel API");
+            
+                    successAlert("User logged in successfully")
+                    /* We wait for the API calls to be fulfilled */
+                    setTimeout(function(){
+                        window.location.href = "/profile";
+                    }, 3000);
+                }
+        )}
         else {
-            response.json().then(errorMessage => {
-                document.getElementById("alert").innerHTML = 
-                `
-                <div class="alert alert-success" role="alert">
-                    <h5 class="alert-heading">${errorMessage}</h5>
-                </div>
-                `
+            result.json().then(res => {
+                dangerAlert(res.message)
                 return
             })
-            
         }
-    })
-    .catch(error => console.log('Error: ', error));
-
-/* window.location.href doesn't wait for the function getFromMarvel to finish 
-document.getElementById("alert").innerHTML = `
-<div class="alert alert-success" role="alert">
-    <h5 class="alert-heading">Successfully logged in</h5>
-</div>`
-
-setTimeout(function() {
-    window.location.href = "/profile";
-}, 4000);
-*/
+    })              
+}
